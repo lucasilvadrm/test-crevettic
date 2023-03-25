22976dc3923f51d2c7125ef745184e6ead9c04f8
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-25 10:42:03 -0300

    fix: remove unused route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import BasicModal from '../components/Modal';
 import Home from '../pages/Home';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/newclient" element={<BasicModal />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
