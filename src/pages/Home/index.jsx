6a7e3875bcf4be8c221f6d00a96888dc1e83438b
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-25 10:45:56 -0300

    feat: removing sales and adding components in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import { StyledPaper } from './styles';
-import Sales from '../../components/Sales';
+import FormContainer from '../../components/Form/FormContainer';
+import BasicModal from '../../components/Modal';
+import FormSale from '../../components/Form/FormSale';
 
 function Home() {
+  const [openModal, setOpenModal] = useState(false);
+
+  // const handleOpenModal = () => {
+  //   setOpenModal(true);
+  // };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
   return (
     <StyledPaper
       elevation={3}
     >
-      <Sales />
+      <FormContainer title="Dados da Venda">
+        {/* <button type="button" onClick={handleOpenModal}>
+          CHAMA MODAL
+        </button> */}
+        <BasicModal
+          openModal={openModal}
+          onCloseModal={handleCloseModal}
+        />
+        <FormSale />
+      </FormContainer>
     </StyledPaper>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,8 @@
 import { styled as StyledMUI } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
+import styled from 'styled-components';
+
 export const StyledPaper = StyledMUI(Paper)(({ theme }) => ({
   ...theme.typography.fontSize,
   width: '100%',
@@ -10,3 +12,23 @@ export const StyledPaper = StyledMUI(Paper)(({ theme }) => ({
   borderRadius: 20,
   margin: '0 auto',
 }));
+
+export const Option = styled.div`
+  border-top: 1px solid #ddd;
+  width: 100%;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  padding-left: 16px;
+  font-size: 13px;
+
+  span {
+    margin-right: 8px;
+  }
+
+  small {
+    font-size: inherit;
+    cursor: pointer;
+    color: #0076C6;
+  }
+`;
