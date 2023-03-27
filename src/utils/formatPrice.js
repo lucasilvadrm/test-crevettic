15ce09564e6b3278d13daeb9664ca4cc1bc55517
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 17:06:41 -0300

    feat: testing input mask functions

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,6 +1,13 @@
-export function formatPrice(price) {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(price);
+export function formatPrice(value = '') {
+  const valueString = value.toString().replace('.', ',');
+
+  const [integer, decimal] = valueString.split(',');
+
+  const formatted = integer.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+  if (!decimal) return `R$ ${formatted},00`;
+
+  if (decimal.length === 1) return `R$ ${formatted}, ${decimal[0]}0`;
+
+  return `R$ ${formatted}, ${decimal[0]}${decimal[1]}`;
 }
