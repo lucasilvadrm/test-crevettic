eef1fb4d14a3928b5f6f195d15f3d1feb95e7e24
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 17:07:20 -0300

    fix: removing validation from integer and adding string

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -4,10 +4,9 @@ export const saleSchema = yup.object().shape({
   description: yup.string()
     .min(4, 'A descrição precisa de no mínimo 4 caracteres')
     .required('Campo Obrigatório'),
-  quantity: yup.number().positive('O valor não pode ser negativo')
-    .integer()
+  quantity: yup.string()
     .required('Campo Obrigatório'),
-  price: yup.number().positive('O valor não pode ser negativo')
+  price: yup.string()
     .required('Campo Obrigatório'),
   status: yup.string().required('Campo Obrigatório'),
   client: yup.string().required('Campo Obrigatório'),
