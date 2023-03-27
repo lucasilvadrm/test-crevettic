6ce14b5c87c1ad35fee6b47a5f6631989c34926e
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 20:52:34 -0300

    feat: adding colors to toast

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -10,6 +10,12 @@ export default {
       paper: '#FFFFFF',
       default: '#E5E5E5',
     },
+    danger: {
+      light: '#F97171',
+      main: '#FC5050',
+      dark: '#F63131',
+    },
+    success: '#51CA73',
   },
   borderRadius: {
     component: '20px',
