638cf545fceee0ccdeaec18906a8c36ba7033598
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-26 23:18:21 -0300

    feat: add clientSchema

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -8,7 +8,27 @@ export const saleSchema = yup.object().shape({
     .integer()
     .required('Campo Obrigatório'),
   price: yup.number().positive('O valor não pode ser negativo')
-    .integer()
     .required('Campo Obrigatório'),
   status: yup.string().required('Campo Obrigatório'),
+  client: yup.string().required('Campo Obrigatório'),
+  dateOfSale: yup.date().required('Campo Obrigatório'),
+});
+
+export const clientSchema = yup.object().shape({
+  name: yup.string()
+    .min(3, 'O campo precisa ter mais de 2 caracteres')
+    .max(200, 'O limite de caracteres é 200')
+    .required('Campo Obrigatório'),
+  street: yup.string()
+    .min(3, 'O campo precisa ter mais de 2 caracteres')
+    .required('Campo Obrigatório'),
+  number: yup.number().integer().positive('O valor não pode ser negativo')
+    .required('Campo Obrigatório'),
+  state: yup.string()
+    .required('Campo Obrigatório'),
+  city: yup.string()
+    .min(3, 'O campo precisa ter mais de 2 caracteres')
+    .required('Campo Obrigatório'),
+  phone: yup.string().min(14, 'O campo precisa ter no mínimo de 14 caracteres')
+    .required('Campo Obrigatório'),
 });
