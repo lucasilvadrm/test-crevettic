a95bae295c529d3f04a736d810cbf6aaed7b7384
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 20:49:21 -0300

    feat: add orderBy by name

--- src/services/helpers/ibge.js
+++ src/services/helpers/ibge.js
@@ -1,22 +1,35 @@
 // https://servicodados.ibge.gov.br/api/v1/localidades/estados
+
+import toast from '../../utils/toast';
+
 // https://servicodados.ibge.gov.br/api/v1/localidades/estados/23/municipios
 const BASE_URL = 'https://servicodados.ibge.gov.br/api/v1';
 
 // eslint-disable-next-line consistent-return
 export const fetchStates = async () => {
-  const url = `${BASE_URL}/localidades/estados`;
+  const url = `${BASE_URL}/localidades/estados?orderBy=nome`;
   try {
     const response = await (await fetch(url)).json();
     return response;
-  } catch {}
+  } catch {
+    toast({
+      type: 'danger',
+      text: 'Erro ao obter dados!',
+    });
+  }
 };
 
 // eslint-disable-next-line consistent-return
 export const fetchCities = async (state) => {
   if (!state) return Promise.resolve([]);
-  const url = `${BASE_URL}/localidades/estados/${state}/municipios`;
+  const url = `${BASE_URL}/localidades/estados/${state}/municipios?orderBy=nome`;
   try {
     const response = await (await fetch(url)).json();
     return response;
-  } catch {}
+  } catch {
+    toast({
+      type: 'danger',
+      text: 'Erro ao obter dados!',
+    });
+  }
 };
