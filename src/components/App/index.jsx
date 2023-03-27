709004975adfa745834bced11553714fe913ea9d
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 10:16:09 -0300

    feat: add ModalProvider

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,17 +5,20 @@ import { theme } from '../../assets/styles/themes/customMui';
 import GlobalStyles from '../../assets/styles/global';
 import Layout from '../../layout';
 import { ClientProvider } from '../../contexts/ClientContext';
+import { ModalProvider } from '../../contexts/ModalContext';
 
 function App() {
   return (
-    <ClientProvider>
-      <ThemeProviderSC theme={themeSC}>
-        <GlobalStyles />
-        <ThemeProvider theme={theme}>
-          <Layout />
-        </ThemeProvider>
-      </ThemeProviderSC>
-    </ClientProvider>
+    <ModalProvider>
+      <ClientProvider>
+        <ThemeProviderSC theme={themeSC}>
+          <GlobalStyles />
+          <ThemeProvider theme={theme}>
+            <Layout />
+          </ThemeProvider>
+        </ThemeProviderSC>
+      </ClientProvider>
+    </ModalProvider>
   );
 }
 
