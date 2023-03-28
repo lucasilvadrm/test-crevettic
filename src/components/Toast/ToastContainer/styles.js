ee44f669595a761f1e087d85fff23062e070199e
Lucas Silva <lucassilva.uern@gmail.com>
2023-03-27 21:34:50 -0300

    fix: adjust the position of the toast

--- src/components/Toast/ToastContainer/styles.js
+++ src/components/Toast/ToastContainer/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: fixed;
   z-index: 999;
-  bottom: 48px;
+  /* bottom: 48px; */
+  top: 200px;
   left: 50%;
   transform: translateX(-50%);
 `;
